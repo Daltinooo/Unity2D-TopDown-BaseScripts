64e093a7e39c3161ef1476ef37a3075e28b9fbad
Daltinooo <128945742+Daltinooo@users.noreply.github.com>
2024-03-21 01:01:09 -0400

    Update Gun.cs

--- Scripts/Gun.cs
+++ Scripts/Gun.cs
@@ -3,8 +3,9 @@ using UnityEngine;
 public class Gun : MonoBehaviour
 {
     public GameObject bulletPrefab; // The bullet that this gun shoots
-    public float fireRate; // The rate at which this gun can fire at
-    public float orbitSpeed; // The speed at which the gun rotates towards the mouse
+    [Range(0.1f, 1f)]
+    public float fireRate = 1; // The rate at which this gun can fire at
+    private float orbitSpeed = 10f; // The speed at which the gun rotates towards the mouse
 
     public bool rotatesAroundPlayer = false; // Whether the gun rotates around the player or not
 
